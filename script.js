d23aff172643d5cb7f6d4a35d12657bc1e090945
Praise Jones <94780493+PraiseJones@users.noreply.github.com>
2023-02-17 10:55:47 +0100

    updated

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-const section = document.querySelector('.container');
-const equal = document.querySelector('#equal');
-const colorTwo = document.querySelector('bg').style.color = aqua;
+var equalChangeBg = document.getElementById("equal");
+var colors = [
+    "rgb(255, 28, 11)",
+    "rgb(115, 253, 153)",
+    "rgb(120, 141, 243)",
+    "rgb(19, 241, 100)",
+    "rgb(228, 223, 188)",
+    "rgb(58, 147, 251)",
+    "rgb(27, 177, 88)",
+    "rgb(232, 104, 199)",
+    "rgb(219, 81, 9)",
+    "rgb(161, 173, 119)",
+    "rgb(245, 81, 48)",
+    "rgb(246, 243, 239)",
+];
 
-var newColor;
+equalChangeBg.addEventListener("click", function () {
+  var randomColor = colors[Math.floor(Math.random() * colors.length)];
+  document.body.style.backgroundColor = randomColor;
 
-equal.addEventListener('click', (e)=> {
-    e.preventDefault();
-    section.style.background = colorTwo;
-})
+  var changeOperator = document.getElementsByClassName("operators");
+
+  for (var i = 0; i < changeOperator.length; i++) {
+    changeOperator[i].style.color = randomColor;
+  }
+
+  var borderChange = document.getElementById("display");
+
+  borderChange.style.borderColor = randomColor;
+});
